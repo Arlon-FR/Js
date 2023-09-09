9c89e28e3cdc9b5b836f20138cd95dabcc537450
unknown <arlonr28@gmail.com>
2023-09-09 01:42:06 -0400

    Pojetos do curso de Js

---
+++ Calculos.js
@@ -0,0 +1,11 @@
+var a = 10
+// pode-se resolver além do convencional ex: a = 10 + 10
+a += 10
+console.log(a)
+//Vai dar o mesmo resultado
+
+
+var b = 5
+b--
+console.log(b) /*esse modelo serve para aumentar ou diminuir 1, b++ ou ++b, 
+o que muda é antes e o outro é depois o resultado*/
